a74d24bdde5050edd73df373bd8315e88939b01d
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2017-11-15 12:27:30 -0500

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Shopify',
-    version='0.1',
+    version='0.2',
     url='https://github.com/fulfilio/flask-shopify',
     license='BSD',
     author='Fulfil.IO Inc.',
